2d3acaeab792f6ea977312a63f37b3924a756eab
kanitaansari <ka2402845@qu.edu.qa>
2026-07-29 13:54:03 +0300

    added vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,7 @@
 import './globals.css';
+
+import { Analytics } from "@vercel/analytics/next"
+
 export const metadata = {
   title: 'QU Course Insights',
   description: 'Know before you enroll'
@@ -7,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+          <Analytics />
+
+      </body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import styles from "./page.module.css"
+
 export default function Home() {
   return (
     <main className={styles.hero}>
